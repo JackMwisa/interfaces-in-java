77c479d5341756d32518ecc8ab3431cfe66cc3a7
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-17 11:58:51 +0300

    Update Assistant.java

--- collegeApp/Assistant.java
+++ collegeApp/Assistant.java
@@ -1,5 +1,29 @@
-package collegeApp;
+package CollegeApp;
 
-public class Assistant {
-    
+public class Assistant extends CollegePerson implements LearningPerson, TeachingPerson {
+    private boolean isGoingToBeAPhD;
+
+    public Assistant(String name, String surname, int collegeId, boolean isGoingToBeAPhD) {
+        super(name, surname, collegeId);
+        this.isGoingToBeAPhD = isGoingToBeAPhD;
+    }
+
+    // Implement the studyAtHome method from LearningPerson interface
+    @Override
+    public void studyAtHome() {
+        System.out.println(name + " " + surname + " is studying at home and preparing for a PhD.");
+    }
+
+    // Implement the teachToOtherPeople method from TeachingPerson interface
+    @Override
+    public void teachToOtherPeople() {
+        System.out.println(name + " " + surname + " is teaching while preparing to become a PhD.");
+    }
+
+    // Override goToCollege method to include PhD status
+    @Override
+    public void goToCollege() {
+        super.goToCollege();
+        System.out.println("Is Going To Be a PhD: " + (isGoingToBeAPhD ? "Yes" : "No"));
+    }
 }
